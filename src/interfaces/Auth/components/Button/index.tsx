4ff2818ef38b73755622606f5b34ebbea9fd6372
mortyhwk <jsphfrdg@gmail.com>
2023-09-17 13:20:40 +0200

    button > ripple effect

--- src/interfaces/Auth/components/Button/index.tsx
+++ src/interfaces/Auth/components/Button/index.tsx
@@ -1,45 +1,70 @@
 import React from 'react'
 import './styles.sass'
-// import styles from './style.module.sass'
 import {ButtonType} from '../../types'
-import {CSSTransition} from "react-transition-group"
-// import styled from "@emotion/styled"
+import Checkbox from '../Checkbox'
+import {CSSTransition} from 'react-transition-group'
 
 interface IButtonProps {
     isShowAnim: boolean
+    arrowState?: string
     text?: string
     style?: { [key: string]: string }
     type: ButtonType
     disabled?: boolean
     onClick?: () => void
-    centeredText?: boolean
+    checked?: boolean
+    setChecked?: (newValue: boolean) => void
 }
-
-// const ButtonStyled = styled.button`
-//   background-color: hotpink;
-// `
 
 const Button: React.FC<IButtonProps> = ({
                                             isShowAnim,
+                                            arrowState,
                                             text,
                                             style = {},
                                             type,
                                             disabled,
                                             onClick = () => {
                                             },
-                                            centeredText
+                                            checked,
+                                            setChecked
                                         }) => {
     return (
-        <CSSTransition in={isShowAnim} timeout={{enter: 600, exit: 300}} mountOnEnter unmountOnExit classNames='Button'>
-            <div
-                className={`Button${disabled ? ' disabled' : ''} type-${type}${
-                    centeredText ? ' centeredText' : ''
-                }`}
-                style={style}
-                onClick={onClick}
-            >
-                {text}
-            </div>
+        <CSSTransition in={isShowAnim} timeout={{enter: 600, exit: 300}} mountOnEnter unmountOnExit classNames='btn'>
+            <>
+                {type !== ButtonType.TransparentCheckbox ?
+                    (
+                        <div
+                            className={`btn btn-ripple${disabled ? ' disabled' : ''} type-${type}`}
+                            style={style}
+                            onClick={onClick}
+                        >
+                            {text}
+                            {type === 3 && (
+                                <svg className={`arrow-${arrowState}`} xmlns="http://www.w3.org/2000/svg"
+                                     width="25%"
+                                     height="25%"
+                                     viewBox="0 0 28 44.52">
+                                    <use href="#arrow"/>
+                                </svg>
+                            )}
+                        </div>
+                    ) : (
+                        <div
+                            className={`btn btn-ripple${disabled ? ' disabled' : ''} type-${type}`}
+                            style={style}
+                            onClick={() => setChecked(!checked)}
+                        >
+                            <Checkbox
+                                isShowAnim={isShowAnim}
+                                text={text}
+                                checked={checked}
+                                setChecked={setChecked}
+                            />
+                        </div>
+                    )
+                }
+
+            </>
         </CSSTransition>
     )
 }
