85af9e9ffb820976907f750274436f8ef39568e3
mortyhwk <jsphfrdg@gmail.com>
2023-09-17 13:22:31 +0200

    update title

--- src/interfaces/Auth/components/Title/index.tsx
+++ src/interfaces/Auth/components/Title/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import './styles.sass'
+import {CSSTransition} from "react-transition-group";
 
 interface ITitleProps {
+    isShowAnim: boolean
     text: string
     style?: {
         [key: string]: string
     }
 }
 
-const Title: React.FC<ITitleProps> = ({text, style}) => {
+const Title: React.FC<ITitleProps> = ({isShowAnim, text, style}) => {
     return (
-        <div className="Title" style={style}>{text}</div>
+        <CSSTransition in={isShowAnim} timeout={{enter: 600, exit: 300}} mountOnEnter unmountOnExit classNames='Title'>
+            <div className="Title" style={style}>{text}</div>
+        </CSSTransition>
     )
 }
 
